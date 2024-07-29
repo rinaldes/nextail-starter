54c64e4e5bdaa3c332d943e1da51983a29b41d03
Rinaldes Duma <rinaldes.duma@gmail.com>
2024-07-30 02:01:15 +0700

    feat: :sparkles: Add React-hook-form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
-const Home = () => <main>Tehe</main>;
+import ContactUs from "@/containers/ContactUs";
+
+const Home = () => (
+  <main className="w-1/2 mx-auto mt-36">
+    <ContactUs />
+  </main>
+);
 
 export default Home;
