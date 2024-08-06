aed6a87c198761e778e39283c5f349e1b756fa37
Rinaldes Duma <rinaldes.duma@gmail.com>
2024-08-06 21:23:29 +0700

    chore: :package: Change NextUI from Global Package to Individual Package

--- src/providers/NextUIProviders.tsx
+++ src/providers/NextUIProviders.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { NextUIProvider } from "@nextui-org/react";
+import { NextUIProvider } from "@nextui-org/system";
 
 import { ChildType } from "@/shares/types";
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-import { nextui } from "@nextui-org/react";
+import { nextui } from "@nextui-org/theme";
 
 const config: Config = {
   content: [
